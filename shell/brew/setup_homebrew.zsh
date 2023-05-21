6b8c23272bd70eadd55caa48b7e14288eee26deb
lg-labs <105936384+lg-labs@users.noreply.github.com>
2023-05-22 00:04:05 +0200

    Update setup_homebrew.zsh

--- shell/brew/setup_homebrew.zsh
+++ shell/brew/setup_homebrew.zsh
@@ -1,12 +1,10 @@
 #!/usr/bin/env zsh
 
 echo "\n<<< Starting Homebrew Setup >>>\n"
-sudo ln -s /bin/zsh /usr/local/bin
-sudo ln -s /opt/homebrew/bin/zsh /usr/local/bin
 
 if ! exists brew; then
   echo "brew doesn't exists, continuing with install"
-  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+  xcode-select --install /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 fi
 
 brew bundle --verbose
