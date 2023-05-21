a8f538b8853ec2fdbdaa6aaa10b7a1ab52264f47
Luis F. García Q <105936384+luisferlabs@users.noreply.github.com>
2023-05-21 02:06:09 +0200

    add configuration to mac os

--- config/setup_macos.zsh
+++ config/setup_macos.zsh
@@ -42,6 +42,39 @@ defaults write com.apple.finder ShowPathbar -bool true
 ###############################################################################
 # Dock, Dashboard, and hot corners                                            #
 ###############################################################################
+# Enable highlight hover effect for the grid view of a stack (Dock)
+defaults write com.apple.dock mouse-over-hilite-stack -bool true
+
+defaults write com.apple.dock tilesize -int 42
+
+# Change minimize/maximize window effect
+defaults write com.apple.dock mineffect -string "genie"
+
+# Minimize windows into their application’s icon
+defaults write com.apple.dock minimize-to-application -bool false
+
+# Show indicator lights for open applications in the Dock
+defaults write com.apple.dock show-process-indicators -bool true
+
+# Don’t animate opening applications from the Dock
+defaults write com.apple.dock launchanim -bool false
+
+# Speed up Mission Control animations
+defaults write com.apple.dock expose-animation-duration -float 0
+
+# Don’t group windows by application in Mission Control
+defaults write com.apple.dock expose-group-by-app -bool false
+
+# Disable Dashboard
+defaults write com.apple.dashboard mcx-disabled -bool true
+
+# Don’t show Dashboard as a Space
+defaults write com.apple.dock dashboard-in-overlay -bool true
+
+# Don’t automatically rearrange Spaces based on most recent use
+defaults write com.apple.dock mru-spaces -bool false
+
+defaults write com.apple.Dock autohide-delay -float 0.3
 
 # Automatically hide and show the Dock
 defaults write com.apple.dock autohide -bool true
