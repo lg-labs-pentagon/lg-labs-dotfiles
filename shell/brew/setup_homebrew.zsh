fe388653e40c0dfd103e8812b984205f248e6cfc
Luis García <105936384+lg-labs@users.noreply.github.com>
2023-06-11 19:30:51 +0200

    Update setup_homebrew.zsh

--- shell/brew/setup_homebrew.zsh
+++ shell/brew/setup_homebrew.zsh
@@ -6,5 +6,3 @@ if ! exists brew; then
   echo "brew doesn't exists, continuing with install"
   /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 fi
-
-brew bundle --verbose
