7f8789d4624f86a7e5cdff69f10bc02a1697891d
Luis F. García Q <105936384+luisferlabs@users.noreply.github.com>
2023-06-11 20:05:33 +0200

    feat: Use NVM to NodeJS and remove N

--- langs/js/setup_node.zsh
+++ langs/js/setup_node.zsh
@@ -2,6 +2,13 @@
 
 printf "\n<<< Starting NODE Setup >>>\n"
 
+# Other alternative is NVM
+if ! exists nvm; then
+  printf "\n<<< Starting NVM  Setup>>>\n"
+  echo "nvm doesn't exists, continuing with install"
+  curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.38.0/install.sh | bash
+fi
+
 # Node versions are managed with 'n', which is in the Brewfile.
 # See zshrc for N_PREFIX variable.
 # See zshenv for addition to $path array.
@@ -9,7 +16,7 @@ if exists node; then
   echo "Node $(node --version) & NPM $(npm --version) already installed"
 else
   echo "Installing Node & NPM with n..."
-  n latest
+  #n latest
 fi
 
 # Install Global packages
@@ -25,9 +32,4 @@ npm install --global trash-cli
 echo "Global NPM Packages Installed:"
 npm list --global --depth=0
 
-# Other alternative is NVM
-if ! exists nvm; then
-  printf "\n<<< Starting NVM  Setup>>>\n"
-  echo "nvm doesn't exists, continuing with install"
-  #curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.38.0/install.sh | bash
-fi
+
